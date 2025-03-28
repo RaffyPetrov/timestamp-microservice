e0bd93631e59be0556739f2936892a7f5523e8d6
Rafael Petrov <djfanta@abv.bg>
2025-03-28 12:48:17 +0000

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -13,12 +13,16 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 })
 
-app.get("/json", (req, res) => {
+// app.get("/json", (req, res) => {
+//     res.json({"message": "Hello json"})
+// })
+
+app.get("/json" , (req, res) => {
+    if (process.env["MESSAGE_STYLE"] == "uppercase") {
+        res.json({"message": "HELLO JSON"})
+    } else 
     res.json({"message": "Hello json"})
 })
-
-
-
 
 
 
